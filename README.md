ccce62a0772015adb40b1747f126a2e16d1ba5e4
eibethdc <eibeth.delgado@gmail.com>
2024-04-07 20:05:45 -0400

    iniciando repositorio

---
+++ README.md
@@ -0,0 +1,2 @@
+# Desafío - Happy Cake
+En este desafio se validan los conocimientos adquiridos de React Router. Para lo cual se creó una aplicación que ofrece la navegación de 2 vistas basadas en rutas: Home y Contacto.
---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./nav.css"
+
+function Nav() {
+    return (
+        <header>
+            <nav>
+                <Link to='/' style={{textDecoration:'none', color:'white'}}>🏠Home</Link>
+                <Link to='/contact' style={{textDecoration:'none', color:'white'}}>📒Contact</Link>
+            </nav>
+        <div>
+            <p>Happy Cake🍰</p>
+        </div>
+        </header>
+    )
+}
+
+export default Nav
